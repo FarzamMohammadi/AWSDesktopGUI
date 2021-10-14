ccb9e3d6f394317ba07fda25d9328bf696e2d65b
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2021-10-14 01:25:40 -0400

    Navigation template added

---
+++ DesktopAWSGUI/eBookReader/ViewModels/LoginViewModel.cs
@@ -0,0 +1,24 @@
+﻿using eBookReader.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace eBookReader.ViewModels
+{
+    public class LoginViewModel : BaseViewModel, IPageViewModel
+    {
+        private ICommand _goTo2;
+
+        public ICommand GoTo2
+        {
+            get
+            {
+                return _goTo2 ?? (_goTo2 = new RelayCommand(x =>
+                {
+                    Mediator.Notify("GoTo2Screen", "");
+                }));
+            }
+        }
+    }
+}
---
+++ DesktopAWSGUI/eBookReader/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,21 @@
+﻿using eBookReader.Commands;
+using System.Windows.Input;
+
+namespace eBookReader.ViewModels
+{
+    class ProfileViewModel : BaseViewModel, IPageViewModel
+    {
+        private ICommand _goTo1;
+
+        public ICommand GoTo1
+        {
+            get
+            {
+                return _goTo1 ?? (_goTo1 = new RelayCommand(x =>
+                {
+                    Mediator.Notify("GoTo1Screen", "");
+                }));
+            }
+        }
+    }
+}
